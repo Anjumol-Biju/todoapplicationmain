ea2bb6c08a8e7eec5b458374caf4930b2b956ca2
Anju <anjumolbiju998@gmail.com>
2024-01-30 21:17:50 +0530

    django model creation new

--- todoapp/models.py
+++ todoapp/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     tittle=models.CharField(max_length=200)
-    created_date=models.DateTimeField(auto_now_add=True,blamk=True)
+    created_date=models.DateTimeField(auto_now_add=True,blank=True)
     user=models.CharField(max_length=200)
     options=(
-        ("complete","complete")
-        ("pending","pending")
+        ("complete","complete"),
+        ("pending","pending"),
         ("inprogress","inprogress")
     )
     status=models.CharField(max_length=200,choices=options,default="pending")
